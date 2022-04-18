37734ed8d8df18312968a627fa44cc303db6a10a
BhashkarChirag <bhaskarchirag7@gmail.com>
2022-04-18 09:24:31 +0530

    made some changes

--- tipcalc.js
+++ tipcalc.js
@@ -1,9 +1,9 @@
 function calculateTip() {
   let amount = document.getElementById("amount").value;
   let persons = document.getElementById("persons").value;
-  let service = document.getElementById("services").value;
+  let services = document.getElementById("services").value;
 
-  console.log(service);
+  console.log(services);
   if (amount === "" && service === "Select") {
     alert("Please enter valid values");
     return;
@@ -12,7 +12,7 @@ function calculateTip() {
   if (persons === "1") document.getElementById("each").style.display = "none";
   else document.getElementById("each").style.display = "block";
 
-  let total = (amount * service) / persons;
+  let total = (amount * (services/100)) / persons;
   total = total.toFixed(2);
 
   document.querySelector(".tip").style.display = "block";
